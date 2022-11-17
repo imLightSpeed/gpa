1dc8cb98f5dd7617fabe18fce2fd5a776ac05924
Luke Sanborn <lukesanborn@outlook.com>
2022-11-16 23:09:02 -0500

    basic runtime stuff

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import argparse
+from src import GPA
+import json 
+
+path = "tests/data/test_grades copy.json"
+with open(path,"r") as file:
+    courses = json.load(file)
+gpa =  GPA(courses)
+print(f"CUW: {gpa.cumulative_unweighted_gpa():.3f}")
+print(f"CW: {gpa.cumulative_weighted_gpa():.3f}")
+
+print(gpa.yearly_unweighted_gpa().head(20))
+print(gpa.yearly_weighted_gpa().head(20))
+
+
+path = "tests/data/test_grades.json"
+with open(path,"r") as file:
+    courses = json.load(file)
+gpa =  GPA(courses)
+print(f"CUW: {gpa.cumulative_unweighted_gpa():.3f}")
+print(f"CW: {gpa.cumulative_weighted_gpa():.3f}")
+
+print(gpa.yearly_unweighted_gpa().head(20))
+print(gpa.yearly_weighted_gpa().head(20))
