a99f83fcdcdcc45b92c46d45a06438be5b3adc47
Luke Sanborn <lukesanborn@outlook.com>
2022-11-17 21:39:54 -0500

    sorting and stuff

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from src import GPA
-import json 
+import json
 import pandas as pd
 
-students = {"Luke S":"data/luke_grades.json","Mikhail R":"data/mikhail_grades.json","Paul N":"data/paul_grades.json"}
-yearly_unweighted, yearly_weighted,cumulative_unweighted,cumulative_weighted  = [],[],[],[]
+students = {"Luke S": "data/luke_grades.json",
+            "Mikhail R": "data/mikhail_grades.json", "Paul N": "data/pauln_grades.json", "JQ":"data/john_grades.json"}
+yearly_unweighted, yearly_weighted, cumulative_unweighted, cumulative_weighted = [], [], [], []
 
 for s, path in students.items():
-    with open(path,"r") as file:
+    with open(path, "r") as file:
         courses = json.load(file)
-    gpa =  GPA(courses)
-    yuw, yw,cuw,cw = gpa.stats(name=s, show=False)
+    gpa = GPA(courses)
+    yuw, yw, cuw, cw = gpa.stats(name=s, show=False)
     yearly_unweighted.append(yuw)
     yearly_weighted.append(yw)
     cumulative_unweighted.append(cuw)
     cumulative_weighted.append(cw)
 
-print(f"Cumulative Unweigted GPA:\n\n{pd.concat(cumulative_unweighted)}\n\n")
-print(f"Cumulative Weighted GPA:\n\n{pd.concat(cumulative_weighted)}\n\n")
+print(f"Cumulative Unweigted GPA:\n\n{pd.concat(cumulative_unweighted).sort_values()}\n\n")
+print(f"Cumulative Weighted GPA:\n\n{pd.concat(cumulative_weighted).sort_values()}\n\n")
 
 print(f"Yearly Unweigted GPA:\n\n{pd.concat(yearly_unweighted,axis=1)}\n\n")
 print(f"Yearly Weighted GPA:\n\n{pd.concat(yearly_weighted,axis=1)}\n\n")
--- src/gpa.py
+++ src/gpa.py
@@ -16,12 +16,12 @@ class GPA:
     def _weighted_gpa(self, df):
         credits = (df["weighted_grade"]*df["gpa_weight"]).sum()
         points = df["gpa_weight"].sum()
-        return round(credits/points,3)
+        return round(credits/points, 3)
 
     def _unweighted_gpa(self, df):
         credits = (df["unweighted_grade"]*df["gpa_weight"]).sum()
         points = df["gpa_weight"].sum()
-        return round(credits/points,3)
+        return round(credits/points, 3)
 
     def yearly_unweighted_gpa(self):
         s = self.df.groupby("year").apply(self._unweighted_gpa)
@@ -30,23 +30,22 @@ class GPA:
     def yearly_weighted_gpa(self):
         s = self.df.groupby("year").apply(self._weighted_gpa)
         return pd.Series(s, name="yearly_weighted_gpa")
-        
-    def stats(self, name="",show=True):
+
+    def stats(self, name="", show=True):
         df1 = self.yearly_unweighted_gpa()
         df2 = self.yearly_weighted_gpa()
         df1.name = name
-        df2.name=name
+        df2.name = name
         if show:
             print("All Classes")
             print(self.df)
-    
-            print(f"Cumulative Unweigted GPA: {self.cumulative_unweighted_gpa():.3f}")
-            print(f"Cumulative Weighted GPA: {self.cumulative_weighted_gpa():.3f}")
-    
-            print("Yearly GPA Breakdown")
-            print(pd.concat([df1,df2],axis=1))
 
-        return df1,df2,pd.Series({name:self.cumulative_unweighted_gpa()}),pd.Series({name:self.cumulative_weighted_gpa()},name=name)
-        
-        
-        
+            print(
+                f"Cumulative Unweigted GPA: {self.cumulative_unweighted_gpa():.3f}")
+            print(
+                f"Cumulative Weighted GPA: {self.cumulative_weighted_gpa():.3f}")
+
+            print("Yearly GPA Breakdown")
+            print(pd.concat([df1, df2], axis=1))
+
+        return df1, df2, pd.Series({name: self.cumulative_unweighted_gpa()}), pd.Series({name: self.cumulative_weighted_gpa()}, name=name)
